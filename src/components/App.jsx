8842d35048e2b5537656bb8b790f382fcdc863cb
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-12-06 13:23:02 +0600

    setting up images type

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import "../styles/index.css";
 import Recipes from "./Recipes";
+import heroBackground from "../assets/hero-background.png";
 
 const App = () => {
     return (
         <>
-            <section className="hero"></section>
+            <section>
+                <img src={heroBackground} alt="" />
+            </section>
             <main>
                 <section>
                     <h1>This is Mahfujur Rahman</h1>
